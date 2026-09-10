d9aac3e5b1db325867cd811de76c8321b394c679
BG <arthurazulbg@gmail.com>
2026-09-10 15:31:44 -0300

    Integrar intro, musica por Screen e SFX globais

--- com/orion/echoes/mars/EchoesMarsGame.java
+++ com/orion/echoes/mars/EchoesMarsGame.java
@@ -1,16 +1,27 @@
 package com.orion.echoes.mars;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 import managers.AssetManager;
 import managers.AudioManager;
+import screens.GameScreen;
+import screens.IntroScreen;
 import screens.MenuScreen;
+import screens.MissionScreen;
+import screens.TitanScreen;
+import screens.lua.LunarScreen;
 
 public class EchoesMarsGame extends Game {
 
     private SpriteBatch batch;
     private AssetManager assets;
     private AudioManager audio;
+
+    private float stepTimer = 0f;
 
     @Override
     public void create() {
@@ -21,7 +32,91 @@ public class EchoesMarsGame extends Game {
         audio = new AudioManager();
         audio.load();
 
-        setScreen(new MenuScreen(this, batch, assets));
+        setScreen(new IntroScreen(this, batch, assets));
+    }
+
+    @Override
+    public void setScreen(Screen screen) {
+        Screen previous = getScreen();
+
+        if (audio != null) {
+            audio.stopMusic();
+
+            if (previous != null && isPortalTransition(previous, screen)) {
+                audio.playPortal();
+            } else if (previous != null) {
+                audio.playClick();
+            }
+
+            startMusicFor(screen);
+        }
+
+        super.setScreen(screen);
+    }
+
+    private boolean isPortalTransition(Screen previous, Screen next) {
+        return (previous instanceof LunarScreen && next instanceof GameScreen)
+            || (previous instanceof GameScreen && next instanceof TitanScreen);
+    }
+
+    private void startMusicFor(Screen screen) {
+        if (screen instanceof MenuScreen || screen instanceof MissionScreen) {
+            audio.playMusic("music/tema_menu.ogg", 0.35f);
+        } else if (screen instanceof LunarScreen) {
+            audio.playMusic("music/tema_lua.ogg", 0.35f);
+        } else if (screen instanceof GameScreen) {
+            audio.playMusic("music/tema_marte.ogg", 0.35f);
+        } else if (screen instanceof TitanScreen) {
+            audio.playMusic("music/tema_tita.ogg", 0.35f);
+        }
+    }
+
+    @Override
+    public void render() {
+        handleGlobalSfx(Gdx.graphics.getDeltaTime());
+        super.render();
+    }
+
+    private void handleGlobalSfx(float delta) {
+        if (audio == null || getScreen() == null) {
+            return;
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)
+            || Gdx.input.isKeyJustPressed(Input.Keys.C)) {
+            audio.playClick();
+        }
+
+        if (isGameplayScreen()) {
+            boolean moving = Gdx.input.isKeyPressed(Input.Keys.W)
+                || Gdx.input.isKeyPressed(Input.Keys.A)
+                || Gdx.input.isKeyPressed(Input.Keys.S)
+                || Gdx.input.isKeyPressed(Input.Keys.D)
+                || Gdx.input.isKeyPressed(Input.Keys.UP)
+                || Gdx.input.isKeyPressed(Input.Keys.DOWN)
+                || Gdx.input.isKeyPressed(Input.Keys.LEFT)
+                || Gdx.input.isKeyPressed(Input.Keys.RIGHT);
+
+            if (moving) {
+                stepTimer -= delta;
+                if (stepTimer <= 0f) {
+                    audio.playCollect();
+                    stepTimer = 0.45f;
+                }
+            } else {
+                stepTimer = 0f;
+            }
+
+            if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+                audio.playAlert();
+            }
+        }
+    }
+
+    private boolean isGameplayScreen() {
+        return getScreen() instanceof LunarScreen
+            || getScreen() instanceof GameScreen
+            || getScreen() instanceof TitanScreen;
     }
 
     @Override
