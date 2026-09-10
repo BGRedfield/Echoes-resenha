0c26f0956b131eace72078ecfb02f7dba82b200c
BG <arthurazulbg@gmail.com>
2026-09-10 15:00:50 -0300

    Integrate AudioManager into game

--- com/orion/echoes/mars/EchoesMarsGame.java
+++ com/orion/echoes/mars/EchoesMarsGame.java
@@ -3,25 +3,31 @@ package com.orion.echoes.mars;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import managers.AssetManager;
+import managers.AudioManager;
 import screens.MenuScreen;
 
 public class EchoesMarsGame extends Game {
 
     private SpriteBatch batch;
     private AssetManager assets;
+    private AudioManager audio;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         assets = new AssetManager();
-        assets.load(); //carrega todas as texturas
+        assets.load();
 
-        //Começa no Menu
+        audio = new AudioManager();
+        audio.load();
+
         setScreen(new MenuScreen(this, batch, assets));
     }
 
     @Override
     public void dispose() {
+        if (screen != null) screen.dispose();
+        if (audio != null) audio.dispose();
         if (batch != null) batch.dispose();
         if (assets != null) assets.dispose();
         super.dispose();
@@ -31,7 +37,11 @@ public class EchoesMarsGame extends Game {
         return batch;
     }
 
-    public AssetManager getAssets(){
+    public AssetManager getAssets() {
         return assets;
+    }
+
+    public AudioManager getAudio() {
+        return audio;
     }
 }
